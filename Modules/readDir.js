216f467d56c8d66e4889968a165a4792592c90be
Reilley <reilleyford@gmail.com>
2018-01-30 15:56:37 -0500

    finished initial script

--- Modules/readDir.js
+++ Modules/readDir.js
@@ -3,13 +3,13 @@ const nodeDir = require('node-dir');
   Reads through all directories and sub directories.
   adds file paths to the passed array.
 */
-module.exports.readDir = (directory, arr, regexp) => {
+module.exports.readDir = (directory, arr) => {
   console.log('Reading Directory');
   try {
     let filePaths = nodeDir.files(directory, {sync:true});
     filePaths.forEach(file => {
-      if(file.match(regexp))
-        arr.push(file);
+      arr.push(file);
+      console.log(file);
     });
   } catch (TypeError) {
     console.error('ERROR: Empty Directory in Input');
---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require('fs-extra');
+const dirs = require('./Modules/readDir.js');
+const readline = require('readline');
+
+const inputDir = 'Testing/Test Files/';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let showName = 'null';
+let fileArr = [];
+
+dirs.readDir(inputDir, fileArr);
+
+let i = 1;
+renameFile = (name, arr) => {
+  arr.forEach(file => {
+    if(i < 10) {
+      newName = inputDir + name + 'E0' + i + '.mkv';
+    }
+    else {
+      newName = inputDir + name + 'E' + i + '.mkv';
+    }
+    fs.rename(file, newName);
+    i++;
+  });
+}
+
+rl.question('Show Name and Season (eg. Legion S01): ', (answer) => {
+  renameFile(answer, fileArr);
+  rl.close();
+});
