34b705c0b911abddbe44a68d34ade8645185e679
Reilley <reilleyford@gmail.com>
2018-03-02 14:59:10 -0500

    added input validation, file type choice

--- Modules/readDir.js
+++ Modules/readDir.js
@@ -1,16 +1,20 @@
 const nodeDir = require('node-dir');
 /*
   Reads through all directories and sub directories.
-  adds file paths to the passed array.
+  adds mp4 and mkv file paths to the passed array.
 */
 module.exports.readDir = (directory, arr) => {
   console.log('Reading Directory');
   try {
     let filePaths = nodeDir.files(directory, {sync:true});
     filePaths.forEach(file => {
-      if(file.split('.').pop() != 'DS_Store')
+      let fileExt = file.split('.').pop();
+      if(fileExt == 'mkv' || fileExt == 'mp4') {
         arr.push(file);
-      console.log(file);
+        console.log('Pushing file: ' + file);
+      } else {
+        console.log('Skipping: ' + file);
+      }
     });
   } catch (TypeError) {
     console.error('ERROR: Empty Directory in Input');
--- app.js
+++ app.js
@@ -3,28 +3,62 @@ const fs = require('fs-extra');
 const dirs = require('./Modules/readDir.js');
 const readline = require('readline');
 
-const inputDir = 'Input/';
+const INPUT = 'Input/';
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 let fileArr = [];
-dirs.readDir(inputDir, fileArr);
+//populating fileArr with filepaths from readDir.js module
+dirs.readDir(INPUT, fileArr);
 
-let i = 1;
-renameFile = (name, arr) => {
+/**
+ * renameFile function will iterate over an array of file file paths
+ * and rename each one. Called by getShowName.
+**/
+renameFile = (name, arr, fileType) => {
+  let i = 1;
   arr.forEach(file => {
     if(i < 10)
-      newName = inputDir + name + 'E0' + i + '.mkv';
+      newName = INPUT + name + 'E0' + i + '.' + fileType;
     else
-      newName = inputDir + name + 'E' + i + '.mkv';
+      newName = INPUT + name + 'E' + i + '.' + fileType;
+    console.log('Renaming: ' + file + ' to File: ' + newName);
     fs.rename(file, newName);
     i++;
   });
 }
 
-rl.question('Show Name and Season (eg. Legion S01): ', (answer) => {
-  renameFile(answer, fileArr);
-  rl.close();
-});
+/**
+ * grabs user input from cmd line, mkv or mp4 and then passes response to
+ * getShowName() function.
+**/
+(getFileType = () => {
+  rl.question('Filetype: (eg. mkv or mp4)', (type) => {
+    if(type != '') {
+      type = type.toLowerCase();
+      getShowName(type);
+    } else {
+      console.log('Filetype cannot be null');
+      getFileType();
+    }
+  });
+})();
+
+/**
+ * grabs user input (show name and season number) then passes response,
+ * fileArr with all filepaths and finally the fileType to renameFile function
+ * called by the first userinput
+**/
+getShowName = (fileType) => {
+  rl.question('Show Name and Season (eg. Legion S01): ', (answer) => {
+    if(answer != '') {
+      renameFile(answer, fileArr, fileType);
+      rl.close();
+    } else {
+        console.log('Show name cannot be blank');
+        getShowName(fileType);
+    }
+  });
+}
